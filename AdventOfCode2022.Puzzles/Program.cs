59587fb2889ea7b27b56c28e9a4cec0980610cc8
Logan Farci <farci.logan@gmail.com>
2022-12-10 13:52:13 +0100

    Initial commit

---
+++ AdventOfCode2022.Puzzles/Program.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode.Kit.Console;
+using AdventOfCode2022.Puzzles.Day01;
+
+namespace AdventOfCode2022.Puzzles;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var app = AdventOfCodeConsole.Instance;
+
+        app.StartYear(2022, year => {
+            year.Submit<ExamplePuzzle>().ForDay(1);
+        });
+
+        app.Run(args);
+    }
+}
